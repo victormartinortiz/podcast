43f463fac6bc16b5e82886e2984cfc3aa5a26ab7
Martin Ortiz <martinortizpalacio@gmail.com>
2020-03-12 22:33:20 -0300

    ADD: babelrc and prettierrc & refactor index, _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,11 +10,13 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
-        <GlobalStyle />
-        <PageNProgress color='#1db954' showSpinner={true} />
-        <Component {...pageProps} />
-      </>
+      <html lang='es'>
+        <>
+          <GlobalStyle />
+          <PageNProgress color='#1db954' showSpinner={true} />
+          <Component {...pageProps} />
+        </>
+      </html>
     );
   }
 }
