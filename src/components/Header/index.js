745c6462fdf68aa27a568df323d3560247b6d6a3
Martin Ortiz <martinortizpalacio@gmail.com>
2020-03-14 22:17:04 -0300

    FIX: update dependencies and typo in <Header />

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { HeaderStyle, HeaderLink } from './styles'
+import { HeaderStyle, HeaderLink } from './styles';
 
 const Header = ({ pageHeader }) => (
   <HeaderStyle>
